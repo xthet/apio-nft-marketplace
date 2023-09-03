67927261448a77e384d7a3f0a3304ddb45065e32
xapski2671 <xapski2671@gmail.com>
2023-09-03 06:49:00 +0100

    request for wallet

--- contexts/connection.jsx
+++ contexts/connection.jsx
@@ -4,102 +4,120 @@ import { createContext, useState, useEffect } from "react"
 
 const ConnectionContext = createContext(null)
 
-function ConnectionProvider ({ children }) {
-  const router = useRouter()
-  const [hasMetamask, setHasMetamask] = useState(false)
-  const [isConnected, setIsConnected] = useState(false)
-  const [chainId, setChainId] = useState("31337")
-  const [signer, setSigner] = useState(null)
-  const [provider, setProvider] = useState(null)
-  const [account, setAccount] = useState("0x0")
-  const [balance, setBalance] = useState("00")
-  const [defSigner, setDefSigner] = useState()
+function ConnectionProvider({ children }) {
+	const router = useRouter()
+	const [hasMetamask, setHasMetamask] = useState(false)
+	const [isConnected, setIsConnected] = useState(false)
+	const [chainId, setChainId] = useState("31337")
+	const [signer, setSigner] = useState(null)
+	const [provider, setProvider] = useState(null)
+	const [account, setAccount] = useState("0x0")
+	const [balance, setBalance] = useState("00")
+	const [defSigner, setDefSigner] = useState()
 
-  async function connect()
-  {
-    if (typeof window.ethereum !== "undefined") {
-      try {
-        await window.ethereum.request({ method: "eth_requestAccounts" })
-        setIsConnected(true)
-        const provider = new ethers.providers.Web3Provider(window.ethereum)
-        provider && setProvider(provider)
-        const signer = await provider.getSigner()
-        setSigner(signer)
-        const { chainId } = await provider.getNetwork()
-        setChainId(chainId.toString())
-      } catch (e) {
-        console.log(e)
-      }
-    } else {
-      setIsConnected(false)
-    }
-  }
+	async function connect() {
+		if (typeof window.ethereum !== "undefined") {
+			try {
+				await window.ethereum.request({ method: "eth_requestAccounts" })
+				setIsConnected(true)
+				const provider = new ethers.providers.Web3Provider(window.ethereum)
+				provider && setProvider(provider)
+				const signer = await provider.getSigner()
+				setSigner(signer)
+				const { chainId } = await provider.getNetwork()
+				setChainId(chainId.toString())
+			} catch (e) {
+				console.log(e)
+			}
+		} else {
+			setIsConnected(false)
+		}
+	}
 
-  async function updateUI()
-  {
-    if(typeof window.ethereum !== "undefined")
-    {
-      try {
-        const accounts = await window.ethereum.request({ method: "eth_accounts" })
-        if(accounts.length)
-        {
-          setIsConnected(true)
-          const provider = new ethers.providers.Web3Provider(window.ethereum)
-          provider && setProvider(provider)
-          const balance = await provider.getBalance(accounts[0])
-          const signer = await provider.getSigner()
-          setSigner(signer)
-          const { chainId } = await provider.getNetwork()
-          if(chainId.toString() !== "11155111")
-          {
-            await window.ethereum.request({
-              method: "wallet_switchEthereumChain",
-              params: [{ chainId: "0xAA36A7" }],
-            })
-            console.log("You have switched to the right network")
-          }
-          setAccount(accounts[0])
-          setBalance(ethers.utils.formatEther(balance))
-          setChainId(chainId.toString())
-        }
-        else{setIsConnected(false)}
+	async function updateUI() {
+		if (typeof window.ethereum !== "undefined") {
+			try {
+				const accounts = await window.ethereum.request({
+					method: "eth_accounts",
+				})
+				if (accounts.length) {
+					setIsConnected(true)
+					const provider = new ethers.providers.Web3Provider(window.ethereum)
+					provider && setProvider(provider)
+					const balance = await provider.getBalance(accounts[0])
+					const signer = await provider.getSigner()
+					setSigner(signer)
+					const { chainId } = await provider.getNetwork()
+					if (chainId.toString() !== "11155111") {
+						await window.ethereum.request({
+							method: "wallet_switchEthereumChain",
+							params: [{ chainId: "0xAA36A7" }],
+						})
+						console.log("You have switched to the right network")
+					}
+					setAccount(accounts[0])
+					setBalance(ethers.utils.formatEther(balance))
+					setChainId(chainId.toString())
+				} else {
+					setIsConnected(false)
+				}
+			} catch (e) {
+				console.log(e)
+			}
 
-      } catch (e) {
-        console.log(e)
-      }
+			window.ethereum.on("chainChanged", () => {
+				updateUI()
+			})
+			window.ethereum.on("accountsChanged", () => {
+				updateUI()
+			})
+		}
+	}
 
-      window.ethereum.on("chainChanged", () => {
-        updateUI()
-      })
-      window.ethereum.on("accountsChanged", () => {
-        updateUI()
-      })
-    }
-  }
+	async function initDefWall() {
+		const provider = new ethers.providers.getDefaultProvider(
+			process.env.NEXT_PUBLIC_SEPOLIA_RPC_URL
+		)
+		const wallet = new ethers.Wallet(
+			process.env.NEXT_PUBLIC_FORMIC_PRIVATE_KEY,
+			provider
+		)
+		setDefSigner(wallet)
+	}
 
-  async function initDefWall(){
-    const provider = new ethers.providers.getDefaultProvider(process.env.NEXT_PUBLIC_SEPOLIA_RPC_URL)
-    const wallet = new ethers.Wallet(process.env.NEXT_PUBLIC_FORMIC_PRIVATE_KEY, provider)
-    setDefSigner(wallet)
-  }
+	useEffect(() => {
+		if (typeof window.ethereum !== "undefined") {
+			setHasMetamask(true)
+			updateUI().catch((e) => console.log(e))
+			initDefWall().catch((e) => console.log(e))
+		} else {
+			if (
+				confirm(
+					"You need a Web3 wallet to use this site,\nWould you like to install Metamask"
+				)
+			) {
+				router.push("https://metamask.io/")
+			}
+		}
+	}, [account, chainId])
 
-  useEffect(() => {
-    if (typeof window.ethereum !== "undefined") {
-      setHasMetamask(true)
-      updateUI().catch(e=>console.log(e))
-      initDefWall().catch(e=>console.log(e))
-    }else{if(confirm("You need a Metamask wallet to use this site,\nWould you like to install Metamask")){
-      router.push("https://metamask.io/")
-    }else{alert("Please install Metamask ;)")}}
-  }, [account, chainId])
+	const payload = {
+		hasMetamask,
+		isConnected,
+		chainId,
+		signer,
+		account,
+		connect,
+		balance,
+		provider,
+		defSigner,
+	}
 
-  const payload = { hasMetamask, isConnected, chainId, signer, account, connect, balance, provider, defSigner }
-
-  return (
-    <ConnectionContext.Provider value={payload}>
-      {children}
-    </ConnectionContext.Provider>
-  )
+	return (
+		<ConnectionContext.Provider value={payload}>
+			{children}
+		</ConnectionContext.Provider>
+	)
 }
 
 export { ConnectionContext, ConnectionProvider }
